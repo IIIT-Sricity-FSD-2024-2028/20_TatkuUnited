e90a394ccd8dcf263cc976c1b5938f15f72684de
Shri Charan <rshricharan29@gmail.com>
2026-08-25 22:41:49 +0530

    switch to pnpm

--- back-end/src/modules/auth/auth.module.ts
+++ back-end/src/modules/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { StringValue } from 'ms';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
+import { StringValue } from 'ms';
 
 @Module({
   imports: [
