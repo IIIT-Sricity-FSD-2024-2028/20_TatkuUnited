6f5a64182ab40f3934923c1a06dd503dbf1e775a
Shri Charan <rshricharan29@gmail.com>
2026-08-28 13:04:13 +0530

    create page templates

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Landing from "./pages/LandingPage"
+import Register from "./pages/RegisterPage"
+import Login from "./pages/LoginPage"
+
 function App() {
-  return <div className="flex h-screen justify-center items-center">
-    <div className="text-6xl text-blue-500 border-8 border-blue-500">Hello world</div>
-  </div>;
+  return (<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/auth">
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+      <Route path="*" element={<div>404! Page Not Found.</div>} />
+    </Routes>
+  </BrowserRouter>);
 }
 
 export default App;
